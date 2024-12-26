781c50f3d9c96100ca2670074ed2a16dd779d57e
Uran1u <milanjevic1a2b@example.com>
2024-12-26 19:15:51 +0000

    remaking everything new beggining

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,52 @@
+
 module Main where
-import Data 
-import Mecanics
-import Render
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss
+import System.Random (mkStdGen)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = play
+    displayMode
+    backgroundColor
+    stepRate
+    initialWorld
+    drawWorld
+    handleEvent
+    handleStep
+
+displayMode :: World -> Display
+displayMode word = InWindow "Snake" (resolution world) (0, 0)
+
+
+backgroundColor :: Color
+backgroundColor = black
+
+
+stepRate :: Int
+stepRate = 1
+
+
+initialWorld :: World
+initialWorld = NewWorld
+    {resolution :: (640, 480)
+    ,
+    }
+
+
+drawWorld :: World -> Picture
+drawWorld world = blank
+
+
+handleEvent :: Event -> World -> World
+handleEvent event world = world
+
+
+handleStep :: Float -> World -> World
+handleStep time world = world
+
+
+--
+data World = NewWorld
+    {resolution :: (Int,Int)
+    ,
+    } deriving (Eq, Ord, Read, Show)
